f9de21add3ac057a63a754b407aced9029f54cdf
Eddie Vasquez <eddie.p.vasquez@gmail.com>
2015-10-06 20:34:40 -0500

    removing uneeded fluff

--- authenticate_rax.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-resize; clear; echo `date`; echo "Hello, please enter a username: "; read user ; echo "Enter your auth key please: "; read authkey ;echo "Your Token is: "; curl -H"X-Auth-User: $user" -H"X-Auth-Key: $authkey" https://auth.api.rackspacecloud.com/v1.0 -D - 2>&1 |awk '/Auth-Token:/{print $2}';
